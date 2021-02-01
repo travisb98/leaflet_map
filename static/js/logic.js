8838bcb931eb13bfa0f132c946d4c771eb033bfb
unknown <travisbostick@gmail.com>
2021-01-31 18:58:02 -0600

    added pop ups to each circle

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,8 @@
 
 /// making the map
 var myMap = L.map('mapid',{
-  center:[40.7128,-74.0059],
-  zoom:8
+  center:[39.7128,-120.0059],
+  zoom:5
 });
 
 // Adding tile layer
@@ -15,9 +15,7 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
-
-var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson"
-
+var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson";
 
 var startTime ="2014-01-01";
 
@@ -31,11 +29,95 @@ var maxLatitude ="48.74894534";
 
 var minLatitude="25.16517337";
 
-var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" + 
-  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+
+// //// query 
+// d3.json(queryUrl, function(data){
+//   console.log(data);
+
+// });
 
 
-d3.json(queryUrl, function(data){
-  console.log(data);
+// var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" + 
+//   "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+
+/// function for plotting markers
+function makeMarkers(data){
+  for (var x = 0; x < data.features.length; x++){
+    var lat = data.features[x].geometry.coordinates[1];
+    var lon = data.features[x].geometry.coordinates[0];
+    L.marker([lat,lon]).addTo(myMap);
+  };
+}
+
+
+////// should I make this a layer instead of just adding them all to the map individually
+function makeCircles(data){
+  for (var x = 0; x < data.features.length; x++){
+
+    /// maybe define these variables outside the function and pass them into the function since we'll use them for multiple functions????
+    var lat = data.features[x].geometry.coordinates[1];
+    var lon = data.features[x].geometry.coordinates[0];
+    var depth = data.features[x].geometry.coordinates[2];
+
+    var property = data.features[x].properties;
+
+    var magnitude = data.features[x].properties.mag;
+
+    console.log(property);
+
+    // console.log(magnitude);
+
+    L.circle([lat,lon],{
+      fillOpacity: .75,
+      //// need to change this color based on the size of the circle?
+      color: "white",
+      radius: depth * 5000
+
+    }).bindPopup('<h3> Magnitude: </h3>'+magnitude).addTo(myMap);
+
+  }
+
+
+}
+
+
+
+//// query 
+d3.json(queryUrl+"&starttime="+startTime+"&endtime="+endTime+"&maxlongitude="+maxLongitude+"&minlongitude="+minLongitude+"&maxlatitude="+maxLatitude+"&minlatitude="+minLatitude, function(data){
+  // console.log(data);
+
+
+  // ///unpacking json
+  // var bbox = data.bbox;
+  // var features = data.features;
+  // var metadata = data.metadata;
+  // console.log(data.bbox);
+  // console.log(data.features);
+  // console.log(data.metadata);
+  // console.log(data.features.length);
+  // console.log(data.features);
+  // console.log(data.features[0].geometry);
+  // console.log(data.features[0].properties);
+  // L.marker([39.7128,-120.0059]).addTo(myMap);
+
+
+  // makeMarkers(data);
+
+  makeCircles(data);
+  console.log(data)
+
+
+  // function makeQuakes (features){
+
+  //   function eachFeat (feature,layer){
+
+  //   }
+
+  // };
+
 
 });
+
+
+
+
